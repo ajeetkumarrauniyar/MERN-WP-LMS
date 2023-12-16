9ab7755a6ec40a13e16da545ea8103f86e3526a9
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2023-12-16 17:20:43 +0530

    Theme and Color Configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-const App = () => {
-  return (
-    <div className='app'>App</div>
-  )
-}
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-export default App
+const App = () => {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;

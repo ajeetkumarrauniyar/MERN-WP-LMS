12bb16bf7f2db659e88b954d994fdea5a4a3e4d4
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2023-12-16 17:29:56 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
+// Import necessary components and functions from the theme file and Material-UI
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./pages/global/Topbar";
 
 const App = () => {
+  // Retrieve the current theme and color mode using the useMode hook
   const [theme, colorMode] = useMode();
 
   return (
+    // Provide the color mode context to the components
     <ColorModeContext.Provider value={colorMode}>
+      {/* Apply the theme to the entire application using ThemeProvider */}
       <ThemeProvider theme={theme}>
+        {/* Apply global baseline CSS styles */}
         <CssBaseline />
         <div className="app">
           <main className="content">
+            <Topbar />
           </main>
         </div>
       </ThemeProvider>

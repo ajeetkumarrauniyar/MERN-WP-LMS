4d8deaef2bb6360d9e223d89ffb5dc3f5082b124
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2023-12-30 16:26:56 +0530

    Add: alias and Server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __filename = import.meta.url;
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["js-big-decimal"],
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      // "@utils": path.resolve(__dirname, "src/utils"),
+      "@data": path.resolve(__dirname, "src/data"),
+    },
+  },
+  server: {
+    port: 8000,
+  },
+  
+});

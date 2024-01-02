a89730bed3c1601d2e0f4bf49fbc6b38e4bb5beb
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-02 11:06:14 +0530

    Calendar Initalized

---
+++ client/src/pages/calender/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import CommonHeader from "../../components/calendar/headerToolbar";
+import ListView from "../../components/calendar/listView"; 
+import DayView from "../../components/calendar/dayView";
+import MonthView from "../../components/calendar/monthView";
+import WeekView from "../../components/calendar/weekView";
+
+const App = () => {
+  const [currentView, setCurrentView] = useState("month");
+
+  const handleSelectView = (view) => {
+    setCurrentView(view);
+  };
+
+  return (
+    <div>
+      {/* <CommonHeader onSelectView={handleSelectView} /> */}
+      {/* Render view based on the selectedView */}
+      {currentView === "list" && <ListView />}
+      {currentView === "day" && <DayView />}
+      {currentView === "week" && <WeekView />}
+      {currentView === "month" && <MonthView />}
+    </div>
+  );
+};
+
+export default App;

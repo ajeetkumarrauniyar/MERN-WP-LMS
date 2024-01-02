51b1bd0122ce8cebcb636f50e5fbd85443f851cf
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-02 11:11:29 +0530

    Update
    Public & Private Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import CalenderScene from "./pages/calender";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
 // import FAQ from "./pages/faq";
-// import { SignedOut, SignedIn } from "@clerk/clerk-react";
+import { SignedOut, SignedIn, SignInButton } from "@clerk/clerk-react";
 
 const App = () => {
   // Retrieve the current theme and color mode using the useMode hook
@@ -29,28 +29,29 @@ const App = () => {
         {/* Apply global baseline CSS styles */}
         <CssBaseline />
         <div className="app">
-          {/* <SignedOut>
+          <SignedOut>
             <p>This content is public. Only signed out users can see this.</p>
-          </SignedOut> */}
-          {/* <SignedIn> */}
-          <SidebarComponent />
-          <main className="content">
-            <Topbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/edit-profile" element={<EditProfileForm />} />
-              <Route path="/calendar" element={<CalenderScene />} />
-              {/* <Route path="/form" element={<Form />} /> */}
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
-            </Routes>
-          </main>
-          {/* </SignedIn> */}
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <SidebarComponent />
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/edit-profile" element={<EditProfileForm />} />
+                <Route path="/calendar" element={<CalenderScene />} />
+                {/* <Route path="/form" element={<Form />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/faq" element={<FAQ />} /> */}
+              </Routes>
+            </main>
+          </SignedIn>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>

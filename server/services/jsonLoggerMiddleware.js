9b7b0465f77bdf24551a001e1fb6777945adcdbb
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-04 12:52:02 +0530

    Cleanup

--- server/services/jsonLoggerMiddleware.js
+++
@@ -1,7 +0,0 @@
-// Log the incoming JSON data (Middleware)
-const jsonLoggerMiddleware = (req, res, next) => {
-  console.log("Incoming JSON data:", req.body);
-  next();
-};
-
-module.exports = jsonLoggerMiddleware;

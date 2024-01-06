d1bd69589a125648a780e51c2411910bc2aaf125
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-07 02:19:49 +0530

    useUser
    Getting User Details

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,12 +1,18 @@
 // Import necessary components and functions from React, Material-UI, and external libraries
 import { Box } from "@mui/material";
-import Header from "../../components/Header";
+import { useUser } from "@clerk/clerk-react";
 
 const Dashboard = () => {
+  // useUser hook to get the details about the logged in user
+  const { user } = useUser();
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="Dashboard" />
+        {user ? (
+          <div>
+            Hello {user.primaryEmailAddress.emailAddress}
+          </div>
+        ) : null}
       </Box>
     </Box>
   );

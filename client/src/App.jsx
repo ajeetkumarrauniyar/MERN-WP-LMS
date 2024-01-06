07033c1d0ff64129feb31cc8000da49ecda9f651
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-07 02:15:54 +0530

    SignedOut, SignedIn Implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,52 +9,54 @@ import Team from "./pages/team";
 import Contacts from "./pages/contacts";
 import Invoices from "./pages/invoices";
 import EditProfileForm from "./pages/edit-profile";
-import CalenderScene from "./pages/calender";
+import CalendarScene from "./pages/calendar";
 // import Bar from "./pages/bar";
 // import Form from "./pages/form";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
 // import FAQ from "./pages/faq";
-import { SignedOut, SignedIn, SignInButton } from "@clerk/clerk-react";
+import { SignedOut, SignedIn } from "@clerk/clerk-react";
+import Homepage from "./pages/homepage";
 
 const App = () => {
   // Retrieve the current theme and color mode using the useMode hook
   const [theme, colorMode] = useMode();
 
   return (
-    // Provide the color mode context to the components
-    <ColorModeContext.Provider value={colorMode}>
-      {/* Apply the theme to the entire application using ThemeProvider */}
-      <ThemeProvider theme={theme}>
-        {/* Apply global baseline CSS styles */}
-        <CssBaseline />
-        <div className="app">
+    <>
+      {/* Provide the color mode context to the components */}
+      <ColorModeContext.Provider value={colorMode}>
+        {/* Apply the theme to the entire application using ThemeProvider */}
+        <ThemeProvider theme={theme}>
+          {/* Apply global baseline CSS styles */}
+          <CssBaseline />
           <SignedOut>
-            <p>This content is public. Only signed out users can see this.</p>
-            <SignInButton />
+            <Homepage />
           </SignedOut>
-          <SignedIn>
-            <SidebarComponent />
-            <main className="content">
-              <Topbar />
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/team" element={<Team />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/invoices" element={<Invoices />} />
-                <Route path="/edit-profile" element={<EditProfileForm />} />
-                <Route path="/calendar" element={<CalenderScene />} />
-                {/* <Route path="/form" element={<Form />} /> */}
-                {/* <Route path="/bar" element={<Bar />} /> */}
-                {/* <Route path="/pie" element={<Pie />} /> */}
-                {/* <Route path="/line" element={<Line />} /> */}
-                {/* <Route path="/faq" element={<FAQ />} /> */}
-              </Routes>
-            </main>
-          </SignedIn>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+          <div className="app">
+            <SignedIn>
+              <SidebarComponent />
+              <main className="content">
+                <Topbar />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/team" element={<Team />} />
+                  <Route path="/contacts" element={<Contacts />} />
+                  <Route path="/invoices" element={<Invoices />} />
+                  <Route path="/edit-profile" element={<EditProfileForm />} />
+                  <Route path="/calendar" element={<CalendarScene />} />
+                  {/* <Route path="/form" element={<Form />} /> */}
+                  {/* <Route path="/bar" element={<Bar />} /> */}
+                  {/* <Route path="/pie" element={<Pie />} /> */}
+                  {/* <Route path="/line" element={<Line />} /> */}
+                  {/* <Route path="/faq" element={<FAQ />} /> */}
+                </Routes>
+              </main>
+            </SignedIn>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
   );
 };
 

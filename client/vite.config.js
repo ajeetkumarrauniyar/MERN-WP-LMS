a22bd4374383f17e1f9f9d2402db4fcd1262ad1f
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-07 19:39:27 +0530

    Bug Fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,5 @@
-// import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// const __filename = import.meta.url;
-// const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({

350a72bc055609175c93af3e8aad59964aec1185
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-07 19:48:34 +0530

    Refactor

--- client/src/components/global/Topbar.jsx
+++ client/src/components/global/Topbar.jsx
@@ -12,12 +12,37 @@ import {
   SettingsOutlined,
   ShoppingCartOutlined,
 } from "@mui/icons-material";
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton, useClerk } from "@clerk/clerk-react";
+
+// SignUpButton component for sign-up action
+function SignUpButton() {
+  const clerk = useClerk();
+
+  return (
+    <Button
+      variant="contained"
+      className="sign-up-btn"
+      onClick={() => clerk.openSignUp({})}
+    >
+      Sign up
+    </Button>
+  );
+}
+
+// SignInButton component for sign-in action
+function SignInButton() {
+  const clerk = useClerk();
+
+  return (
+    <Button
+      variant="contained"
+      className="sign-in-btn"
+      onClick={() => clerk.openSignIn({})}
+    >
+      Sign in
+    </Button>
+  );
+}
 
 const Topbar = () => {
   // Access the current theme and color mode using Material-UI hooks
@@ -28,76 +53,82 @@ const Topbar = () => {
   const colorMode = useContext(ColorModeContext);
 
   return (
-    // Topbar container with flex layout and space between items
-    <Box display="flex" justifyContent="space-between" p={2}>
-      {/* SearchBar */}
-      <Box
-        display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
-      >
-        {/* Input field for search with a placeholder */}
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+    <header>
+      <nav>
+        <Box display="flex" justifyContent="space-between" p={2}>
+          {/* SearchBar */}
+          <Box
+            display="flex"
+            backgroundColor={colors.primary[400]}
+            borderRadius="3px"
+          >
+            {/* Input field for search with a placeholder */}
+            <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
 
-        {/* Search icon button */}
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchOutlined />
-        </IconButton>
-      </Box>
+            {/* Search icon button */}
+            <IconButton type="button" sx={{ p: 1 }}>
+              <SearchOutlined />
+            </IconButton>
+          </Box>
 
-      {/* Icons */}
-      <Box display="flex">
-        {/* Toggle between light and dark mode */}
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlined />
-          ) : (
-            <LightModeOutlined />
-          )}
-        </IconButton>
-
-        {/* Messages icon */}
-        <IconButton>
-          <Badge badgeContent={0} max={9} color="info">
-            <MessageOutlined />
-          </Badge>
-        </IconButton>
-
-        {/* Notification icon */}
-        <IconButton>
-          <Badge badgeContent={2} max={9} color="info">
-            <NotificationsOutlined />
-          </Badge>
-        </IconButton>
-
-        {/* Cart icon */}
-        <IconButton>
-          <Badge badgeContent={2} max={9} color="info">
-            <ShoppingCartOutlined />
-          </Badge>
-        </IconButton>
-
-        {/* Settings icon */}
-        <IconButton>
-          <SettingsOutlined />
-        </IconButton>
-
-        {/* User  */}
-        {/* Renders a "Log In" button  when the user is signed out.  */}
-        <SignedOut>
-          <SignInButton>
-            <Button variant="contained" className={"inputButton"}>
-              Log In
-            </Button>
-          </SignInButton>
-        </SignedOut>
-
-        {/* Renders a UserButton component when the user is signed in.  */}
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Box>
-    </Box>
+          {/* Icons */}
+          <Box display="flex">
+            {/* Toggle between light and dark mode */}
+            <IconButton onClick={colorMode.toggleColorMode}>
+              {theme.palette.mode === "dark" ? (
+                <DarkModeOutlined />
+              ) : (
+                <LightModeOutlined />
+              )}
+            </IconButton>
+            {/* Messages icon */}
+            <IconButton>
+              <Badge badgeContent={0} max={9} color="info">
+                <MessageOutlined />
+              </Badge>
+            </IconButton>
+            {/* Notification icon */}
+            <IconButton>
+              <Badge badgeContent={2} max={9} color="info">
+                <NotificationsOutlined />
+              </Badge>
+            </IconButton>
+            {/* Cart icon */}
+            <IconButton>
+              <Badge badgeContent={2} max={9} color="info">
+                <ShoppingCartOutlined />
+              </Badge>
+            </IconButton>
+            {/* Settings icon */}
+            <IconButton>
+              <SettingsOutlined />
+            </IconButton>
+            {/* User actions */}
+            {/* Renders a "Sign Up" and "Log In" button when the user is signed out. */}
+            <SignedOut>
+              <ul>
+                <li>
+                  {/* <Button variant="contained" className={"inputButton"}>
+                  Sign Up
+                </Button> */}
+                  <SignUpButton />
+                </li>
+                <li>
+                  {/* <Button variant="contained" className={"inputButton"}>
+                  Log In
+                </Button> */}
+                  <SignInButton />
+                </li>
+              </ul>
+            </SignedOut>
+            {/* Renders a UserButton component when the user is signed in. */}
+            <SignedIn>
+              <UserButton afterSignOutUrl="/" />
+            </SignedIn>
+          </Box>
+        </Box>
+      </nav>
+    </header>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,17 +6,17 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
+console.log("Vite", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
         <App />
-      </ClerkProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 );

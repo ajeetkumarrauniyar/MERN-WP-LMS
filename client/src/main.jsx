852345e8e017f41959fb351019ea424135eed399
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-01-08 12:07:50 +0530

    Refactor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./assets/styles/index.css";
-import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import NoMatch from "./components/global/NoMatch.jsx";
+
+import "./assets/styles/index.css";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-console.log("Vite", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
+
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
